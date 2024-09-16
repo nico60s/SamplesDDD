4453a80fea5a82d9bea1302bf35379fc031daf09
nico60s <nico_60s@hotmail.com>
2024-09-15 21:12:47 -0300

    init

---
+++ TiendaOnline/TiendaOnline.Api/Controllers/Products/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TiendaOnline.Api.Controllers.Products
+{
+    [Route("api/v{version}/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route(".json")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok(id);
+        }
+        [HttpGet]
+        public IActionResult GetAll() { return Ok(); }
+    }
+}
